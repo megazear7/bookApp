33a6b08b58638cf7d481125f9c500c8f8b37940e
megazear7 <alexlockhart7@gmail.com>
2014-02-14 13:15:22 -0500

    added to the books controller and added views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,43 @@
 class BooksController < ApplicationController
+
+  def new
+    if user_signed_in? 
+      @user = current_user
+    else
+      redirect_to new_user_session_path
+    end
+
+    @book = Book.new
+  end
+
+  def create
+    if user_signed_in? 
+      @user = current_user
+    else
+      redirect_to new_user_session_path
+    end
+
+    @book = Book.create(book_params)
+    @book.save
+    redirect_to book_path(@book)
+  end
+
+  def show
+    @book = Book.find(book_id) 
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  private
+   def book_params
+      params.require(:book).permit(:title, :author, :short_description, :long_description)
+   end
+
+  private
+    def book_id
+      params.require(:id)
+    end
+
 end
